f70a4fe857d3927ecc2a68f9b8610395b40b3b29
Fergus Neary <fergusneary@gmail.com>
2019-10-09 20:33:05 +0100

    HackerRank Exercise Java Stdin and Stdout I

---
+++ HackerrankScanner.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HackerrankScanner {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int a = keyboard.nextInt();
+        int b = keyboard.nextInt();
+        int c = keyboard.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        keyboard.close();
+
+    }
+}
